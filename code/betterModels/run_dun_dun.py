ead14970ba84e248e69654b82b5301d5cd2aace6
birthboi <70106649+birthboi@users.noreply.github.com>
2021-05-08 11:11:13 -0400

    finished but not compatible

--- code/betterModels/run_dun_dun.py
+++ code/betterModels/run_dun_dun.py
@@ -1,7 +1,10 @@
-from classModelBase import baseClass
+from classModelBase import modelBase
 from modelsC import model
 
 b = model[b]
 i = model[i]
 s = model[s]
 
+modelClass = modelBase(b, i, s)
+
+modelClass.printAll()
