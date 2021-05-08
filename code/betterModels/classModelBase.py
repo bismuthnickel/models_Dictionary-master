b13de334c500028c0830abdba9707fc1420b6874
birthboi <70106649+birthboi@users.noreply.github.com>
2021-05-08 11:07:16 -0400

    finished base thing
    but not compatible with anything

--- code/betterModels/classModelBase.py
+++ code/betterModels/classModelBase.py
@@ -1,3 +1,8 @@
 class modelBase:
-  def __init__(self):
-    print("k")
+  def __init__(self, b, i, s):
+    self.b = b
+    self.i = i
+    self.s = s
+    
+  def printAll(self):
+    print("Boolean:'{}', Integer:'{}', String:'{}'".format(self.b, self.i, self.s)
